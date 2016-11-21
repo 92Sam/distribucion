9a1128d1dea64bd5a6a5b2ee5997e9782c68815f
ECORTES <cortesc.198@gmail.com>
2016-11-21 01:11:13 -0400

    eliminacion de scripts

--- script/ecortes/cliente_cliente_datos.sql
+++
@@ -1,5 +0,0 @@
-
-
-
-
-ALTER TABLE `cliente` ADD `agente_retencion` BOOLEAN NULL DEFAULT NULL AFTER `id_zona`, ADD `tipo_cliente` INT NOT NULL AFTER `id_zona`, ADD `linea_credito_valor` DECIMAL NULL DEFAULT NULL AFTER `retencion`, ADD `linea_libre` BOOLEAN NULL DEFAULT NULL AFTER `linea_credito_valor`, ADD `linea_libre_valor` DECIMAL NULL DEFAULT NULL AFTER `linea_libre`, ADD `importe_deuda` DECIMAL NULL DEFAULT NULL AFTER `linea_libre_valor`; 
