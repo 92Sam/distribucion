523ddf62963ec4c83577e965cb6560c9d8d95536
Mario Delgado <mariodavid.delgado@outlook.com>
2016-12-08 18:31:49 -0500

    Cambio de acceso a base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '123456';
-$db['default']['database'] = 'distribucion_proyecto';
+$db['default']['password'] = 'nexus1108';
+$db['default']['database'] = 'distribucion_master';
 
 
 /*
