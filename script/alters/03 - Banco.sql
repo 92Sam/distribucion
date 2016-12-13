4cd368d858798740f982d34db9959c7c2940069f
toni <tonitim13@gmail.com>
2016-12-12 21:34:55 -0500

    un ultimo detalle

--- script/alters/03 - Banco.sql
+++ script/alters/03 - Banco.sql
@@ -7,4 +7,7 @@ ADD COLUMN `pago_data` VARCHAR(100) NULL AFTER `historial_banco_id`;
 ALTER TABLE `historial_pagos_clientes`
 ADD COLUMN `vendedor_id` BIGINT(20) NULL DEFAULT NULL AFTER `pago_data`;
 
+INSERT INTO `metodos_pago` VALUES (5,'CHEQUE',1,'CAJA'),(6,'NOTA DE CREDITO',1,'CAJA'),(7,'RETENCION',1,'CAJA');
+
+
 
