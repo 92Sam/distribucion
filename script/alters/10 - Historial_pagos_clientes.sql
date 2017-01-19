99d4028b39b463860bc7c534214b0b4d60d3abc5
Mario Delgado <mariodavid.delgado@outlook.com>
2017-01-19 17:30:33 -0500

    Reorganizacion de script

--- script/alters/10 - Historial_pagos_clientes.sql
+++ script/alters/10 - Historial_pagos_clientes.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `historial_pagos_clientes`
-CHANGE COLUMN `vendedor_id` `vendedor_id` BIGINT(20) NULL DEFAULT NULL  AFTER `historial_banco_id` ,
 ADD COLUMN `fecha_documento` DATETIME NULL DEFAULT NULL  AFTER `vendedor_id` ;
 
 
