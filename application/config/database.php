94a1730e1f2cf03627fc89e54e8c0999aabf468e
Mario Delgado <mariodavid.delgado@outlook.com>
2017-04-23 13:00:37 -0500

    Base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'distribucion_dev';
+$db['default']['password'] = 'nexus1108';
+$db['default']['database'] = 'distribucion_prod';
 
 
 /*
