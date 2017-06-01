eba8efd090c33065bd778e97a945a4447fd21edc
Antonio <tonitim13@gmail.com>
2017-05-31 19:14:17 -0500

    correccion de kardex

--- application/models/kardex/kardex_model.php
+++ application/models/kardex/kardex_model.php
@@ -85,17 +85,25 @@ class kardex_model extends CI_Model
 
         $kardex_inicial = $this->db->get()->row();
 
-        $where['producto_id'] = $producto_id;
+
+        //$this->db->join('producto','kardex.producto_id=producto.producto_id');
+//        $this->db->join('unidades', 'kardex.unidad_id=unidades.id_unidad', 'join');
+//        $kardex = $this->db->get_where('kardex', $where)->result();
+
+        $this->db->select('*')->from('kardex')
+            ->join('unidades', 'kardex.unidad_id=unidades.id_unidad');
+
+
+        $this->db->where('producto_id', $producto_id);
         if ($local_id != false)
-            $where['local_id'] = $local_id;
+            $this->db->where('local_id', $local_id);
 
         if ($mes != false && $year != false) {
-            $where['fecha >='] = $year . '-' . sumCod($mes, 2) . '-01';
-            $where['fecha <='] = $year . '-' . sumCod($mes, 2) . '-' . last_day($year, sumCod($mes, 2));
+            $this->db->where('fecha >=', $year . '-' . sumCod($mes, 2) . '-01 00:00:00');
+            $this->db->where('fecha <=', $year . '-' . sumCod($mes, 2) . '-' . last_day($year, sumCod($mes, 2)) . ' 23:59:59');
         }
-        //$this->db->join('producto','kardex.producto_id=producto.producto_id');
-        $this->db->join('unidades', 'kardex.unidad_id=unidades.id_unidad', 'join');
-        $kardex = $this->db->get_where('kardex', $where)->result();
+
+        $kardex = $this->db->order_by('id', 'ASC')->get()->result();
 
         return array('fiscal' => $kardex, 'inicial' => $kardex_inicial);
     }
@@ -103,7 +111,7 @@ class kardex_model extends CI_Model
     function get_kardex_interno($producto_id, $local_id, $mes, $year)
     {
 
-        $this->db->select('costo_unitario_final, cantidad_final,                                                            total_final,kardex.unidad_id,unidades.nombre_unidad')
+        $this->db->select('costo_unitario_final, cantidad_final, total_final,kardex.unidad_id,unidades.nombre_unidad')
             ->from('kardex')
             ->join('unidades', 'kardex.unidad_id=unidades.id_unidad')
             //->join('producto','kardex.producto_id=producto.producto_id')
@@ -133,7 +141,8 @@ class kardex_model extends CI_Model
         )
             ->from('kardex')
             ->join('unidades', 'kardex.unidad_id=unidades.id_unidad')
-            ->group_by('ref_id, tipo_doc, tipo_operacion')
+            ->join('documento_fiscal', 'documento_fiscal.documento_fiscal_id=kardex.ref_id')
+            ->group_by('documento_fiscal.venta_id, tipo_doc, tipo_operacion')
             ->order_by('id');
 
         $this->db->where('cantidad >', 0);
@@ -142,8 +151,8 @@ class kardex_model extends CI_Model
             $this->db->where('local_id', $local_id);
 
         if ($mes != false && $year != false) {
-            $this->db->where('fecha >=', $year . '-' . sumCod($mes, 2) . '-01');
-            $this->db->where('fecha <=', $year . '-' . sumCod($mes, 2) . '-' . last_day($year, sumCod($mes, 2)));
+            $this->db->where('fecha >=', $year . '-' . sumCod($mes, 2) . '-01 00:00:00');
+            $this->db->where('fecha <=', $year . '-' . sumCod($mes, 2) . '-' . last_day($year, sumCod($mes, 2)) . ' 23:59:59');
         }
 
         $kardex = $this->db->get()->result();
@@ -155,6 +164,7 @@ class kardex_model extends CI_Model
             $total_inicial = $kardex_inicial->total_final;
         }
 
+        $last_costo_unitario = 0;
         foreach ($kardex as $k) {
             $k->referencia = '';
             if ($k->IO == 2) {
@@ -164,11 +174,10 @@ class kardex_model extends CI_Model
                     ->join('documento_venta', 'venta.numero_documento = documento_venta.id_tipo_documento')
                     ->where('venta.venta_id', $df->venta_id)->get()->row();
 
-                if($venta != NULL) {
+                if ($venta != NULL) {
                     $k->serie = $venta->doc_serie;
                     $k->numero = $venta->doc_numero;
-                }
-                else{
+                } else {
                     $k->serie = '';
                     $k->numero = '';
                 }
@@ -178,12 +187,9 @@ class kardex_model extends CI_Model
                 $k->cantidad_final = $cantidad_inicial - $k->cantidad;
                 $cantidad_inicial = $k->cantidad_final;
 
-                $k->total_final = $total_inicial - $k->total;
+                $k->total_final = ($k->cantidad_final * $last_costo_unitario);
                 $total_inicial = $k->total_final;
-                if ($k->cantidad_final != 0)
-                    $k->costo_unitario_final = $k->total_final / $k->cantidad_final;
-                else
-                    $k->costo_unitario_final = 0;
+                $k->costo_unitario_final = $last_costo_unitario;
             } else if ($k->IO == 1) {
                 $k->cantidad_final = $cantidad_inicial + $k->cantidad;
                 $cantidad_inicial = $k->cantidad_final;
@@ -194,6 +200,8 @@ class kardex_model extends CI_Model
                     $k->costo_unitario_final = $k->total_final / $k->cantidad_final;
                 else
                     $k->costo_unitario_final = 0;
+
+                $last_costo_unitario = $k->costo_unitario_final;
             }
         }
 
